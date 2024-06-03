027f741bed409cbc671590ccb40de2d786beaa79
ghervias <gabriel.hervias@gmail.com>
2024-06-03 08:33:24 -0700

    fixes to json formatting

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask, request, jsonify
 import openai
+from werkzeug.utils import secure_filename
+
 from transcript_loader import load_transcript
 from gpt_setup import mpq_to_dict, init_langchain
 from run_query import run_query
 from run_ragas import ragas_eval
 import langchain  # Make sure you import the necessary parts of langchain
+
+ALLOWED_EXTENSIONS = {'txt'}
 
 app = Flask(__name__)
 app.json.sort_keys = False
@@ -18,6 +22,9 @@ openai.api_key = 'your_openai_api_key'
 def index():
     return "Welcome to the GPT-4 Flask API!"
 
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/process', methods=['POST'])
 def process_text():
@@ -28,7 +35,8 @@ def process_text():
     if file.filename == '':
         return jsonify({"error": "No selected file"}), 400
 
-    if file:
+    if file and allowed_file(file.filename):
+        # filename = secure_filename(file.filename)
         text = file.read().decode('utf-8')
 
         # Call the GPT-4 API using your existing code
@@ -37,8 +45,10 @@ def process_text():
         rag_chain = init_langchain(retriever)
         response, trio_list = run_query(rag_chain, mpq_dict, retriever)
         print(response)
+        print(jsonify(response))
+        return jsonify(response)
         # ragas_eval(trio_list)
-    return jsonify(response)
+    return "Invalid file type."
 
 
 
--- run_query.py
+++ run_query.py
@@ -14,7 +14,10 @@ def replace_patterns(input_string):
         r"', '": r'", "',
         r"'}}": r'"}}',
         r"\{'": r'{"',
-        r"\\'s ": r"'s"
+        r"\\'s ": r"'s",
+        r"'}": r'"}',
+        r"':": r'":',
+        r", '": r', "'
     }
 
     # Apply each pattern replacement
@@ -24,13 +27,11 @@ def replace_patterns(input_string):
     return input_string
 
 
-
 def run_query(rag_chain, mpq_dict, retriever):
     query = "Provide a  RFC8259 compliant JSON response following this format without \
     deviation. If there is no relevant information in the transcript, keep the original \
     JSON structure, but fill in the field with 'N/A'. Important: DO NOT provide any \
     information in a field that is not directly relevant. Here is the form: "
-
 
     # Function to retrieve context
     def retrieve_context(query):
@@ -38,30 +39,36 @@ def run_query(rag_chain, mpq_dict, retriever):
 
     trio_list = []
     print("running query...")
+    final_json = {}
     for item in mpq_dict.keys():
+        if(item != "CognitivelyImpairedandIntellectualDisability.json"):
+            continue
         print(item)
-        if(item == "SummaryofEventsLeadingUpToandFollowingMP'sDisappearance.json"):
-            response = rag_chain.invoke(query + str(mpq_dict[item]))
-            context_documents = retriever.get_relevant_documents(query + str(mpq_dict[item]))
-            # print(query + str(mpq_dict[item]))
-            # print(len(context_documents))
-            # print(str(context_documents))
-            context = [doc.page_content for doc in context_documents]
-            # form_json
-            print("pre: " + response)
-            print("post: " + replace_patterns(response))
-            response_json = json.loads(replace_patterns(response))
+        # if(item == "SummaryofEventsLeadingUpToandFollowingMP'sDisappearance.json"):
+        response = rag_chain.invoke(query + str(mpq_dict[item]))
+        context_documents = retriever.get_relevant_documents(query + str(mpq_dict[item]))
+        # print(query + str(mpq_dict[item]))
+        # print(len(context_documents))
+        # print(str(context_documents))
+        context = [doc.page_content for doc in context_documents]
+        # form_json
+        print("pre: " + response)
+        print("post: " + replace_patterns(response))
+        response_json = json.loads(replace_patterns(response))
+        print(response_json)
+        final_json[item.removesuffix('.json')] = response_json
 
-
-            for field_name, field_value in response_json.items():
-                if(field_value == "N/A" or isinstance(field_value, dict)):
-                    continue
-                trio_list.append([mpq_dict[item][field_name], field_value, context])
+        for field_name, field_value in response_json.items():
+            if (field_value == "N/A" or isinstance(field_value, dict)):
+                continue
+            trio_list.append([mpq_dict[item][field_name], field_value, context])
 
     print(response_json)
     print(trio_list)
+    final_json = json.loads(json.dumps(final_json, indent=4))
+
     # print(len(trio_list))
-    #
-    # for query in trio_list:
-    #   print(query[0])
-    return response_json, trio_list
+#
+# for query in trio_list:
+#   print(query[0])
+    return final_json, trio_list
